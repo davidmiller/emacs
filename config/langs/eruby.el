2942dd77de4bbbe50c1ea1d52f7a9880e0013f99
David Miller <David>
2012-08-20 09:07:39 +0100

    Add Ruby configurations

---
+++ config/langs/eruby.el
@@ -0,0 +1,16 @@
+;;
+;; eruby.el
+;;
+;; Commentary:
+;;
+;; Ruby configuration for Emacs
+;;
+
+(defext "\\Rakefile\\'" ruby-mode)
+(defext "\\.god\\'" ruby-mode)
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (set-mode-style ide-style)))
+
+(provide 'eruby)
