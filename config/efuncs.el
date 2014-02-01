23bedc7de2432978ffe317436cbddc67763f6aef
David Miller <david@deadpansincerity.com>
2014-02-01 10:46:59 +0000

    copy-file-name-to-clipboard

--- config/efuncs.el
+++ config/efuncs.el
@@ -228,5 +228,17 @@ be a real filename, not a path."
         (goto-line (read-number "Goto line: ")))
     (linum-mode -1)))
 
+(defun copy-file-name-to-clipboard ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
+
 ;; Code ends
 (provide 'efuncs)
